1a62c917cb02323887ebcdb411fe59b1b23c796e
rishabh gupta <rishabh@vinsol.com>
2020-01-15 15:43:39 +0530

    changed order

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ApplicationRecord
-  has_many :users, dependent: :restrict_with_error
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  has_many :users, dependent: :restrict_with_error  
   scope :customer, -> { find_by(name: 'customer') }
   scope :admin, -> { find_by(name: 'admin') }
 end
