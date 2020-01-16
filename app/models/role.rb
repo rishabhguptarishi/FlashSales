dc0a0b1f791ec9fe111b43b4698544890863d1a7
rishabh gupta <rishabh@vinsol.com>
2020-01-16 09:12:39 +0530

    removing database.yml

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  has_many :users, dependent: :restrict_with_error  
+  has_many :users, dependent: :restrict_with_error
   scope :customer, -> { find_by(name: 'customer') }
   scope :admin, -> { find_by(name: 'admin') }
 end
