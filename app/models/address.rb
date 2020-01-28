af696c32f2d05e459d5f01fffb0776d7a900cd05
rishabh gupta <rishabh@vinsol.com>
2020-01-28 09:38:09 +0530

    basic presenter added

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
+  include BasicPresenter::Concern
   validates :city, :country, :line1, :state, :pincode, presence: true
 
   #FIXME_AB: Address should also belongs to user.
